5a577db268c24777bc3c04ee23b205e72c5cc639
alexey.lazarenko <dzotic9@gmail.com>
2020-11-11 17:42:47 +0200

    JE-53161

--- scripts/validation.sh
+++ scripts/validation.sh
@@ -150,7 +150,7 @@ function validateCustomSSL() {
 
 function runAllChecks(){
    validateExtIP
-   validateDNSSettings
+   #validateDNSSettings
    validateCertBot
    echo "All validations are passed succesfully";
 }
