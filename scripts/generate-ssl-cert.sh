574f0b51c590b6f514be30e25a68b59e54635192
alexey.lazarenko <dzotic9@gmail.com>
2021-01-20 16:56:50 +0200

    Update generate-ssl-cert.sh

--- scripts/generate-ssl-cert.sh
+++ scripts/generate-ssl-cert.sh
@@ -89,11 +89,12 @@ function uploadCerts() {
     echo appdomain = $appdomain
     #Upload 3 certificate files
     uploadresult=$(curl -F "appid=$appid" -F "fid=privkey.pem" -F "file=@${certdir}/privkey.pem" -F "fid=fullchain.pem" -F "file=@${certdir}/chain.pem" -F "fid=cert.pem" -F "file=@${certdir}/cert.pem" http://$primarydomain/xssu/rest/upload)
-
-    #Save urls to certificate files
     echo '' > /tmp/privkey.url
     echo '' > /tmp/fullchain.url
     echo '' > /tmp/cert.url
+    uploadresult=6;
+
+    #Save urls to certificate files
 #    echo $uploadresult | awk -F '{"file":"' '{print $2}' | awk -F ":\"" '{print $1}' | sed 's/","name"//g' > /tmp/privkey.url
 #    echo $uploadresult | awk -F '{"file":"' '{print $3}' | awk -F ":\"" '{print $1}' | sed 's/","name"//g' > /tmp/fullchain.url
 #    echo $uploadresult | awk -F '{"file":"' '{print $4}' | awk -F ":\"" '{print $1}' | sed 's/","name"//g' > /tmp/cert.url
