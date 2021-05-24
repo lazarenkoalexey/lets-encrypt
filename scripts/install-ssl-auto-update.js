50e2c64001cbaa4bffab06fec2a98bf951457807
alexey.lazarenko <dzotic9@gmail.com>
2021-05-24 14:51:37 +0300

    Create install-ssl-auto-update.js

---
+++ scripts/install-ssl-auto-update.js
@@ -0,0 +1,78 @@
+//@url('/${scriptName}')
+
+appUniqueName = "1df11a0c-dcee-453e-9b2c-accd9d95e7e5";
+return jelastic.marketplace.jps.ExecuteAppAction(appid, session, appUniqueName, "auto-update", null, {
+    appAction: "auto-update",
+    action: "update"
+});
+
+
+var action = getParam("action", "install"),
+    patchVersion  = getParam("patchVersion"),
+    token  = getParam("token"),
+    isTask = getParam("task"),
+    baseUrl = "${baseUrl}";
+
+function run() {
+    var SSLManager = use("scripts/ssl-manager.js", {
+        session          : session,
+        isTask           : isTask,
+        baseUrl          : baseUrl,
+        action           : action,
+
+        token            : "${token}",
+        email            : "${email}",
+        baseDir          : "${baseDir}",
+        scriptName       : "${scriptName}",
+        envName          : "${envName}",
+        envDomain        : "${envDomain}",
+
+        appId            : "${appId}",
+        envAppid         : "${envAppid}",
+        nodeId           : "${nodeId}",
+        nodeIp           : "${nodeIp}",
+        nodeGroup        : "${nodeGroup}",
+        customDomains    : "${customDomains}",
+        cronTime         : "${cronTime}",
+        deployHook       : "${deployHook}",
+        deployHookType   : "${deployHookType}",
+        undeployHook     : "${undeployHook}",
+        undeployHookType : "${undeployHookType}",
+        withExtIp        : "${withExtIp}",
+        webroot          : "${webroot}",
+        webrootPath      : "${webrootPath}",
+        fallbackToX1     : "${fallbackToX1}",
+        patchVersion     : "${patchVersion}",
+        test             : "${test}"
+    });
+
+    var resp = SSLManager.auth(token);
+
+    if (resp.result === 0) {
+        resp = SSLManager.invoke(action);
+    }
+
+    jelastic.local.ReturnResult(resp);
+}
+
+function use(script, config) {
+    var Transport = com.hivext.api.core.utils.Transport,
+        body = new Transport().get(baseUrl + "/" + script + "?_r=" + Math.random());
+
+    return new (new Function("return " + body)())(config);
+}
+
+try {
+    run();
+} catch (ex) {
+    var resp = {
+        result : com.hivext.api.Response.ERROR_UNKNOWN,
+        error: "Error: " + toJSON(ex)
+    };
+
+    if (jelastic.marketplace && jelastic.marketplace.console) {
+        jelastic.marketplace.console.WriteLog(appid, signature, "ERROR: " + resp);
+    }
+
+    jelastic.local.ReturnResult(resp);
+}
