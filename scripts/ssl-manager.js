6c9e0e50fe6c46d6ff350fc02995df04b22b838f
alexey.lazarenko <dzotic9@gmail.com>
2021-06-02 17:11:12 +0300

    Update ssl-manager.js

--- scripts/ssl-manager.js
+++ scripts/ssl-manager.js
@@ -578,6 +578,8 @@ function SSLManager(config) {
     };
 
     me.initWebrootMethod = function initWebrootMethod(webroot) {
+        log("config.webroot ->" + config.webroot);
+        log("webroot0 ->" + webroot);
         webroot = isDefined(webroot) ? String(webroot) == "true" : false;
         log("webroot ->" + webroot);
         config.webroot = me.initBoolValue(webroot);
